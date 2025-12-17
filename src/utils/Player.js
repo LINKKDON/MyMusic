523f3ceb751992ab49b236f9f64737ff079ddba9
Linus.T <20582672+LINKKDON@users.noreply.github.com>
2025-12-17 06:56:58 +0000

    fix: resolve prettier linting error in Player.js

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -621,10 +621,13 @@ export default class {
             // 如果 br > 1000，假设是 bps (如 320000)，否则是 kbps (如 320)
             cacheBitrate = br > 1000 ? br : br * 1000;
           } else if (typeof br === 'string') {
-            if (br.includes('bit'))
+            if (br.includes('bit')) {
               cacheBitrate = 1400000; // flac24bit
-            else if (br === 'flac') cacheBitrate = 999000;
-            else cacheBitrate = parseInt(br) * 1000;
+            } else if (br === 'flac') {
+              cacheBitrate = 999000;
+            } else {
+              cacheBitrate = parseInt(br) * 1000;
+            }
           }
 
           cacheTrackSource(track, audioUrl, cacheBitrate || 0, config.name);
